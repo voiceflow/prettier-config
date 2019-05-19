a00aa3918456bfaa2f1eef9a4e7151e2fd424eba
Eric Hacke <eric@xtct.io>
2019-05-19 15:31:33 -0400

    fix: don't force single quotes in prettier, handle in eslint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   printWidth: 150,
   tabWidth: 2,
   semi: true,
-  singleQuote: true,
+  singleQuote: false,
   quoteProps: 'as-needed',
   trailingComma: "es5",
   bracketSpacing: true,
